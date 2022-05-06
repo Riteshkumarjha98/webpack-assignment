d0ae536cb372db7c78782fbf9473cb3fa8a99e2f
Riteshkumarjha98 <soumritjha@gmail.com>
2022-05-07 00:07:37 +0530

    Webpack

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+function navbar(){
+
+    return `
+    <h3><a href="index.html">Home</a></h3>
+    <div id="options">
+        
+        <h3><a href="Ladies.html">Ladies</a></h3>
+        <h3><a href="Mens.html">Mens</a></h3>
+        <h3><a href="Kids.html">kids</a></h3>
+        <input type=search placeholder="searchproducts" id="input"/>
+        <h3><a href="login.html">Log In</a></h3>
+        <h3><a href="signup.html">Sign Up</a></h3>
+        <h3><a href="Cart.html">My Cart</a></h3>
+    </div>
+    `
+}
+
+export  {navbar};
